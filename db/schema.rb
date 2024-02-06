f2b4f959d9040a9d3df614fe1363bb6cf58a07e2
TARUN <tarunrana4221@gmail.com>
2024-02-06 16:13:47 +0530

    added a few gems

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_02_121151) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_06_090652) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "comments", force: :cascade do |t|
